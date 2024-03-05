139394e61f480ee880ef62aa61937bb02e590f7b
Yuan Gonzales <160531194+OdericPipa@users.noreply.github.com>
2024-03-06 02:27:04 +0700

    Update ebalo.py

--- ebalo.py
+++ ebalo.py
@@ -3,6 +3,7 @@
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
+#add these directories to sys.path here. If the directory is relative to the
 # documentation root
 
 import os
